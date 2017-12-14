3c2d08c4fd07950475915a247e0b4939114a5962
Iuliia Kotlenko <iuliiakotlenko@Iuliias-MacBook-Pro.local>
2017-12-14 11:59:58 -0500

    display more info for book

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-        data: [{title: "Little Life", author: "Hanya Yanagihara", price: "$12"}, {title: "Seven Days of Us: A Novel", author: "Francesca Hornak", price:"$17"}, {title: "The Girl on the Train", author: "Paula Hawkins", price:"$10"}]
+        data: [{title: "Little Life", author: "Hanya Yanagihara", price: "$12"}, {title: "Seven Days of Us", author: "Francesca Hornak", price:"$17"}, {title: "The Girl on the Train", author: "Paula Hawkins", price:"$10"}]
     }   
   }
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Book = function(props) {
-    return <li>{props.book.title}</li>
+    return (
+        <li>
+            <p><strong>Title: </strong>{props.book.title}</p>
+            <p><strong>Author: </strong>{props.book.author}</p>
+            <p><strong>Price: </strong>{props.book.price}</p>   
+        </li>
+    )
 }
 
 export default Book;
